51a93ded8d859d7148b94463e7db0dcbcaa40c8f
LuisChaparin <luis.chaparin@gmail.com>
2024-07-02 11:22:11 -0500

    Localization

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
 
-Route::get('/', function () {
+Route::get('/{local}/inicio', function ($locale) {
     return view('inicio');
 });
 
